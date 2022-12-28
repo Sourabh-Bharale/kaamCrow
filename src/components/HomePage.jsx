e8faf08d895a565de12a5f607a9e8714e10f53f0
Sourabh-Bharale <sourabhbharale80@gmail.com>
2022-12-28 14:17:05 +0530

    prevent routing w/o login

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { auth } from '../firebase'
 import { useNavigate } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
+
 import { signOut } from 'firebase/auth' 
 import Button from './Button'
 const HomePage = () => {
     const navigate=useNavigate()
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (!user) {
+                navigate('/')
+            }
+        })
+    }, [])
     const handleSignOut=()=>{
         signOut(auth).then(()=>{
             navigate('/')
